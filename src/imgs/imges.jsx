197281373372846e678f9d1ad5744f12f01b265d
SAVINDU ABEYWICKRAMA <sanjulasavindu@gmail.com>
2024-06-25 22:12:04 +0530

    first commit

---
+++ src/imgs/imges.jsx
@@ -0,0 +1,44 @@
+
+import profilePic from '../imgs/licdn.jpg'
+import linked from '../imgs/linkedin1.png'
+import fb from '../imgs/facebook.png'
+import yt from '../imgs/youtube.png'
+import git from '../imgs/git1.png'
+import iam from '../imgs/whoam.png'
+import skill from '../imgs/skills.png'
+import skill1 from '../imgs/skill1.webp'
+import skill2 from '../imgs/skill2.png'
+
+import skill4 from '../imgs/skill4.png'
+
+import gmail from '../imgs/gmail.webp'
+import phone from '../imgs/phone.png'
+import whats from '../imgs/whats.png'
+import git10 from '../imgs/git10.png'
+
+
+
+const Imgs = {
+
+    profilePic,
+    linked,
+    fb,
+    yt,
+    git,
+    whats,
+    phone,
+    gmail,
+    
+    skill,
+    skill1,
+    skill2,
+    skill4,
+    iam,
+    git10,
+
+
+}
+
+
+
+export default Imgs
