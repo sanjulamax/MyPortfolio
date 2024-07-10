61952523a2f6537ced962db17ec2ab4f699d4842
SAVINDU ABEYWICKRAMA <sanjulasavindu@gmail.com>
2024-07-10 16:06:10 +0530

    change#010

--- src/sec1/sec1.jsx
+++ src/sec1/sec1.jsx
@@ -185,7 +185,7 @@ I have successfully enhanced my skills and knowledge through various certificati
    <li> FUCKERS<a href='/javascript.png' className='ml-[12%] pl-[5px] pr-[5px]  text-green-600 font-mono font-bold rounded-md'>view</a></li>
   <li> SQL<a href='./sql.png' className='ml-[28%] pl-[5px] pr-[5px]   text-green-600 font-mono font-bold rounded-md'>view</a></li>
  
-  <li> HTMLS<a href='https://drive.google.com/file/d/17NGN873bzdHVRTvgHMxhhZbniH9474qx/view?usp=drive_link' className='ml-[23%] pl-[5px] pr-[5px]  text-green-600 font-mono font-bold rounded-md'>view</a></li>
+  <li> HTCS<a href='https://drive.google.com/file/d/17NGN873bzdHVRTvgHMxhhZbniH9474qx/view?usp=drive_link' className='ml-[23%] pl-[5px] pr-[5px]  text-green-600 font-mono font-bold rounded-md'>view</a></li>
   
 
   
